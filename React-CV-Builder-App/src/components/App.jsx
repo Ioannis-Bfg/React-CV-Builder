88f4ba35893f15df8c468e791a8538882c1665ab
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-05-02 15:17:46 +0300

    app is fully functioning , STABLE STATE 1.0

--- React-CV-Builder-App/src/components/App.jsx
+++ React-CV-Builder-App/src/components/App.jsx
@@ -113,6 +113,7 @@ function App() {
     setEducation([]);
     setExperience([]);
     setShowEducationForm(false);
+    setPreviewSrc("");
   };
 
   const handleImageUpload = (event) => {
@@ -147,6 +148,62 @@ function App() {
     });
   }
 
+  const addDefaultProfile = () => {
+    handleReset();
+    setFullName("John Doe");
+    setEmail("john.doe@example.com");
+    setPhone("123-456-7890");
+    setAddress("123 Main Street, Anytown, USA");
+
+    const exampleEducations = [
+      {
+        school: "Example School 1",
+        degree: "Example Degree 1",
+        start_date: "01/2019",
+        end_date: "12/2021",
+        location: "Example Location 1",
+        unique_id: crypto.getRandomValues(new Uint32Array(1))[0],
+      },
+      {
+        school: "Example School 2",
+        degree: "Example Degree 2",
+        start_date: "01/2022",
+        end_date: "12/2024",
+        location: "Example Location 2",
+        unique_id: crypto.getRandomValues(new Uint32Array(1))[0],
+      },
+    ];
+    setEducation(exampleEducations);
+
+    const exampleExperiences = [
+      {
+        company: "Example Company 1",
+        position: "Example Position 1",
+        start_date: "01-2018",
+        end_date: "12-2019",
+        location: "Example Location 1",
+        unique_id: crypto.getRandomValues(new Uint32Array(1))[0],
+      },
+      {
+        company: "Example Company 2",
+        position: "Example Position 2",
+        start_date: "01-2020",
+        end_date: "12-2021",
+        location: "Example Location 2",
+        unique_id: crypto.getRandomValues(new Uint32Array(1))[0],
+      },
+      {
+        company: "Example Company 3",
+        position: "Example Position 3",
+        start_date: "01-2022",
+        end_date: "Present",
+        location: "Example Location 3",
+        unique_id: crypto.getRandomValues(new Uint32Array(1))[0],
+      },
+    ];
+    setExperience(exampleExperiences);
+  };
+
   return (
     <div className="App">
       <div id="inputs">
@@ -173,7 +230,7 @@ function App() {
       <div id="test_buttons">
         <button onClick={addEducationObject}>Add Education Object</button>
         <button onClick={addExperienceObject}>Add Experience Object</button>
-        <button onClick={handleReset}>Reset</button>
+        <button onClick={addDefaultProfile}>Default</button>
       </div>
       <div id="page">
         <CV
@@ -185,6 +242,27 @@ function App() {
           experience={experience}
           previewSrc={previewSrc}
         />
+      </div>
+      <div id="footer">
+        <p>Made by Ioannis-Bfg</p>
+        <a
+          href="https://github.com/Ioannis-Bfg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            width="30"
+            height="30"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
